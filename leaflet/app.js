e5aae4124faa9cb4dd04965eb8b9ed56e255729b
unknown <yjeong19@gmail.com>
2017-12-13 12:55:17 -0500

    test

---
+++ leaflet/app.js
@@ -0,0 +1,18 @@
+var Geocodio = require('geocodio');
+
+var config = {
+    api_key: '7b3d44a014513bbd488455a84a174d15844a3bd'
+}
+
+var geocodio = new Geocodio(config);
+
+
+var address = 'San Francisco, CA 94111'; //this would need to change into user input.
+
+//this function will get the city, state and return a longitude/lat of the location.
+geocodio.get('geocode', {q: address}, function(err, response){
+    if (err) throw err;
+
+    var res = JSON.parse(response);
+    console.log(res.results[0].location);
+});
