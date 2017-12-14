b15bdf22268fd0da4ebc4328f3aca88581edf0f5
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-14 15:48:28 -0500

    added pie chart data call from the server

--- controller/votecontroller.js
+++ controller/votecontroller.js
@@ -24,8 +24,43 @@ router.get("/Washington/:code", function(req, res) {
   }).then(function(data) {
     // console.log(data);
     var hbsObject = data.dataValues;
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render("results", hbsObject)
-  })
-})
+  });
+});
+
+// data grabber for pie chart
+router.get("/api/pie/:code", function(req, res) {
+  db.Washington_state_data.findOne({
+    where: {
+      fips_code: req.params.code
+    }
+  }).then(function(data) {
+    var da = data.dataValues;
+    var obj = {
+      name: da.county,
+      fips_code: da.fips_code,
+      data: {
+        name: "Eligible Voters",
+        children: [
+          {
+            name: "Registered Voters",
+            children: [
+              {
+                name: "Voted",
+                children: [
+                  { name: "Men", size: da.male_ballots_cast },
+                  { name: "Women", size: da.female_ballots_cast }
+                ]
+              },
+              { name: "Non-voter", size: da.total_regis_pop-da.total_ballots_cast}
+            ]
+          },
+          { name: "Unregistered Voters", size: da.total_elig_pop-da.total_regis_pop }
+        ]
+      }
+    };
+    res.json(obj);
+  });
+});
 module.exports = router;
