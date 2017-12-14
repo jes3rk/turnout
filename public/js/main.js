abc30cd3f5def459953a8d001e10dae9735b816c
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-14 15:47:53 -0500

    reformatted and added additional functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,50 @@
+var loginForm = $("form.login");
+var emailInput = $("input#email-input");
+var passwordInput = $("input#password-input");
+
+loginForm.on("submit", function(event) {
+  event.preventDefault();
+  var userData = {
+    email: emailInput.val().trim(),
+    password: passwordInput.val().trim()
+  };
+
+  if (!userData.email || !userData.password) {
+    return;
+  }
+
+  loginUser(userData.email, userData.password);
+  emailInput.val("");
+  passwordInput.val("");
+});
+
+function loginUser(email, password) {
+  $.post("/api/login", {
+    email: email,
+    password: password
+  }).then(function(data) {
+    window.location.replace(data);
+  }).catch(function(err) {
+    console.log(err);
+  });
+};
+
+function pctToPCT(elementID) {
+  // takes a data-nonPct element and turns it into a percentage string
+  $(elementID).text(function() {
+    return ($(this).attr("data-nonPct") * 100).toFixed(2) + "%";
+  });
+};
+
 $(document).ready(function() {
    $(".button-collapse").sideNav();
+
+   if ($('#county-name')) {
+     // console.log($('#county-name').attr("data-fips"));
+     pieData($('#county-name').attr("data-fips"));
+     // $('#turnout').text(function() {
+     //   return ($(this).attr("data-nonPct") * 100).toFixed(2) + "%";
+     // });
+     pctToPCT('#turnout');
+   };
 });
-  var loginForm = $("form.login");
-  var emailInput = $("input#email-input");
-  var passwordInput = $("input#password-input");
-
-  loginForm.on("submit", function(event) {
-    event.preventDefault();
-    var userData = {
-      email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
-    };
-
-    if (!userData.email || !userData.password) {
-      return;
-    }
-
-    loginUser(userData.email, userData.password);
-    emailInput.val("");
-    passwordInput.val("");
-  });
-
-  function loginUser(email, password) {
-    $.post("/api/login", {
-      email: email,
-      password: password
-    }).then(function(data) {
-      window.location.replace(data);
-    }).catch(function(err) {
-      console.log(err);
-    });
-  };
