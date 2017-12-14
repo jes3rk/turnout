8b29b11bdd764c51d9aec3f734dda3a3ad51c04c
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-14 09:30:53 -0500

    starting results sequelize call

--- controller/votecontroller.js
+++ controller/votecontroller.js
@@ -5,10 +5,25 @@ var router = express.Router();
 // Import the model (cat.js) to use its database functions.
 var Washington = require("../models/turnout.js");
 
+// home page router
 router.get("/", function(req, res) {
   // some sequelize call
   var hbsObject;
   res.render("index");
 });
 
+// results page Router
+router.get("/:state/:code", function(req, res) {
+  var state = req.params.state;
+  db.state.findOne({
+    where: {
+      fips_code: req.params.code
+    }
+  }).then(function(data) {
+    var hbsObject = {
+      
+    }
+    res.render("results", hbsObject)
+  })
+})
 module.exports = router;
