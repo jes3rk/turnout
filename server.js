5f42fef202f713c63ced363d09911bb5045d6421
Joseph Schefer <joseph.schefer@gmail.com>
2017-12-18 13:31:45 -0500

    typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 var session = require("express-session");
 var passport = require("./config/passport");
-	
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -31,7 +31,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controller/voteController.js");
+var routes = require("./controller/votecontroller.js");
 app.use("/", routes);
 
 
@@ -43,40 +43,3 @@ db.sequelize.sync().then(function() {
     console.log("App listening on PORT " + PORT);
   });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
