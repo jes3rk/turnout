750a5b79866c3a28f3a078e89fd65d5c81b66042
unknown <yjeong19@gmail.com>
2017-12-18 15:04:55 -0500

    leaflet

--- public/js/buttons.js
+++ public/js/buttons.js
@@ -2,21 +2,22 @@ var testMonkey;
 var counties;
 
 $(document).ready(function(){
-    $('#low').on('click', function(event){
+    $('#lowBtn').on('click', function(event){
         console.log('you clicked low')
-        console.log(event);
+        console.log(this.getAttribute('data-name'));
         testMonkey = 'low';
     });
 });
 
 $(document).ready(function(){
-    $('#med').on('click', function(event){
+    $('#medBtn').on('click', function(event){
         console.log('you clicked low')
+        map.removeLayer(counties);
     });
 });
 
 $(document).ready(function(){
-    $('#high').on('click', function(event){
+    $('#highBtn').on('click', function(event){
         console.log('you clicked low')
 
 
@@ -28,19 +29,26 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('#showAll').on('click', function(event){
         console.log('you clicked low');
-        // counties.remove();
+        if(counties){
+        map.removeLayer(counties)
+        }
         counties = new L.geoJson();
-        counties.addTo(map);
-        geojson = L.geoJson(cdata, {
+        // counties.addTo(map);
+        counties = L.geoJson(cdata, {
             style: style2,
             onEachFeature: onEachFeature
-        }).addTo(map);
-
-        
+        }).addTo(map);       
     });
 });
 
+// $(document).ready(function(){
 
-    $('#infoPage').on('click', function(event){
-        console.log('you clicked low')
-});
+//     $('#infoPage').on('click', function(event){
+//         console.lot('you clicked');
+//         alert('')
+//     });
+// })
+
+function getData(){
+    console.log('test');
+}
